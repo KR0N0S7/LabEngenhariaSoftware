d131e9ac59911e20e820a122a672185188d26cf1
Rogério Massanori Kishi <rogerio.kishi@fatec.sp.gov.br>
2024-05-28 19:01:42 -0300

    app.yml

--- src/test/java/com/boutiquepierrotbleu/boutiquepierrotbleu/BoutiquepierrotbleuApplicationTests.java
+++ src/test/java/com/boutiquepierrotbleu/boutiquepierrotbleu/BoutiquepierrotbleuApplicationTests.java
@@ -3,8 +3,8 @@ package com.boutiquepierrotbleu.boutiquepierrotbleu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BoutiquepierrotbleuApplicationTests {
+@SpringBootTest(classes = BoutiquepierrotbleuApplication.class) // Especifica a classe principal
+public class BoutiquepierrotbleuApplicationTests {
 
 	@Test
 	void contextLoads() {
